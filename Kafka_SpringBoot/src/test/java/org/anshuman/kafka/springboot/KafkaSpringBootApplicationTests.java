e9358c41a2d9bac9c50c7d8b1c9747710809895b
Anshuman Singh <anshuman0220@gmail.com>
2024-02-02 14:57:02 +0530

    Add files via upload

---
+++ Kafka_SpringBoot/src/test/java/org/anshuman/kafka/springboot/KafkaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.anshuman.kafka.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
