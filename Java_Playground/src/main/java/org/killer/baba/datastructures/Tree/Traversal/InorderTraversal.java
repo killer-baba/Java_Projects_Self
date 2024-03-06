539dd8591d742d3ec51af228a946aab5be60b95b
killer_baba <anshuman0220@gmail.com>
2024-03-06 16:54:55 +0530

    Adding Tree Data Structure to the COdeBase 1st Iteration.

---
+++ Java_Playground/src/main/java/org/killer/baba/datastructures/Tree/Traversal/InorderTraversal.java
@@ -0,0 +1,40 @@
+package org.killer.baba.datastructures.Tree.Traversal;
+
+import org.killer.baba.datastructures.Tree.TreeNode;
+
+import java.util.Stack;
+
+public class InorderTraversal {
+
+    public void recursiveTraverse(TreeNode treeNode){
+        if(treeNode == null){
+            return;
+        }
+
+        recursiveTraverse(treeNode.left);
+
+        System.out.print( treeNode.data + "  ");
+
+        recursiveTraverse(treeNode.right);
+    }
+
+    public void iterativeTraverse(TreeNode treeNode){
+        Stack<TreeNode> s = new Stack<>();
+        while (true){
+            if(treeNode == null){
+                if(s.isEmpty()){
+                    break;
+                }
+                treeNode = s.pop();
+                System.out.print( treeNode.data + "  ");
+                treeNode = treeNode.right;
+            }else {
+                s.add(treeNode);
+                treeNode = treeNode.left;
+            }
+        }
+        System.out.println();
+    }
+
+
+}
